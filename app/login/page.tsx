c3f94d9b35d33420c7b95cf54342710f3043cf5c
Edvin Becic <edvinbecic16@outlook.com>
2023-11-20 17:55:12 +0100

    add react-hook-form library and error handling

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,22 +1,71 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
+
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {};
+
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <form className="w-1/4 md:1/5 h-auto flex flex-col gap-4">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-2/3 md:w-1/3 lg:w-1/4 h-auto flex flex-col gap-4"
+      >
         <h1 className="text-center font-bold text-3xl mb-8">Login</h1>
         <div className="flex flex-col gap-2 w-full">
           <Label>Email</Label>
-          <Input type="email" id="email" />
-          <Label className="text-red-500">
-            Please enter a valid email adress
-          </Label>
+          <Input
+            defaultValue=""
+            {...register("email", {
+              required: "Please enter your email adress",
+            })}
+            type="email"
+            id="email"
+          />
+          {errors.email && (
+            <Label className="text-red-500">{errors.email.message}</Label>
+          )}
         </div>
         <div className="flex flex-col gap-2 w-full">
           <Label>Password</Label>
-          <Input type="email" id="email" />
+          <Input
+            defaultValue=""
+            {...register("password", {
+              required: "Please enter your password",
+              minLength: 8,
+              maxLength: 32,
+            })}
+            type="password"
+            id="password"
+          />
+          {errors.password && (
+            <Label className="text-red-500">{errors.password.message}</Label>
+          )}
+          {errors.password?.type === "minLength" && (
+            <Label className="text-red-500">
+              Please enter password that is longer than 8 characters
+            </Label>
+          )}
+          {errors.password?.type === "maxLength" && (
+            <Label className="text-red-500">
+              Please enter password that is shorter than 32 characters
+            </Label>
+          )}
         </div>
         <Button>Login</Button>
       </form>
