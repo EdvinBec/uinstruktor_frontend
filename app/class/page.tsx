ecaecd9179ab09e9c8adf9f3df25508c324dab8e
alkrajnc <aljaz.krajnc@sers.si>
2023-11-25 18:06:41 +0100

    added class fetching based on current user

--- app/class/page.tsx
+++ app/class/page.tsx
@@ -1,5 +1,6 @@
 import { getClasses } from '@/lib/class';
 import React from 'react';
+import { cookies } from 'next/headers';
 
 import {
   Card,
@@ -10,15 +11,27 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import Link from 'next/link';
+import { GetJwtSecretKey, decryptToken } from '@/lib/auth';
+import { compactDecrypt, jwtDecrypt } from 'jose';
+
+async function getCurrentUser() {
+  const cookieStore = cookies();
+  const token = cookieStore.get('token');
+  if (token) {
+    return await decryptToken(token.value);
+  }
+}
 
 const Page = async ({ params }: { params: { slug: string } }) => {
-  const classes = await getClasses('test2');
+  const user = await getCurrentUser();
+  const classes = await getClasses(user as string);
+
   return (
     <div className="p-4">
       <h1 className="text-4xl">Classes:</h1>
-      <div className="grid grid-cols-5 gap-6 p-4">
+      <div className="grid gap-6 p-4">
         {classes.map((subject, index) => (
-          <Link key={index} href={`/class/${subject.classID}`}>
+          <Link key={index} className="" href={`/class/${subject.classID}`}>
             <Card className="">
               <CardHeader>
                 <CardTitle>{subject.className}</CardTitle>
