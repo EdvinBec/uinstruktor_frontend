bfba2a0adfa7a9407ff9075812da72a3372e0221
Edvin Becic <edvinbecic16@outlook.com>
2023-12-07 18:48:17 +0100

    add: footer

--- app/explore/layout.tsx
+++ app/explore/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Footer from "@/components/Footer/Footer";
 import Greeting from "@/components/Greeting";
 import Navbar from "@/components/Navbar/Navbar";
 import Sidebar from "@/components/Sidebar/Sidebar";
@@ -9,12 +10,13 @@ const ExploreLayout = ({ children }: { children: React.ReactNode }) => {
   const auth = useAuth();
 
   return (
-    <div className="flex h-screen">
+    <div className="flex h-full">
       <Sidebar />
-      <div className="flex flex-col w-full h-screen px-4">
+      <div className="flex flex-col w-full h-full">
         <Navbar />
         <Greeting username={auth?.username!} />
         {children}
+        <Footer />
       </div>
     </div>
   );
