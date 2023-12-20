f602fdfee08782f2a65dd114e24743effac06518
alkrajnc <aljaz.krajnc@sers.si>
2023-12-20 19:16:24 +0100

    Fixed class card, added class card banner images

--- next.config.js
+++ next.config.js
@@ -6,6 +6,12 @@ const nextConfig = {
         protocol: "http",
         hostname: "127.0.0.1",
         port: "5000",
+        pathname: "/class/**/*",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "5000",
         pathname: "/user/*",
       },
     ],
