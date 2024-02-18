971ba104d7ba12ac8853fcdaee42f87fd2635787
alkrajnc <aljaz.krajnc@sers.si>
2024-02-18 10:25:19 +0100

    responsive fixes

--- app/course/page.tsx
+++ app/course/page.tsx
@@ -20,20 +20,44 @@ const CoursePage = async () => {
     <>
       <div className="w-full text-neutral-100 bg-neutral-900 rounded-xl p-4 py-6 my-4 mb-8">
         <h3 className="text-2xl text-center">
-          Neveš kje bi začel?{" "}
+          Neveš kje bi začel? <br className="md:hidden" />
           <span className="font-medium underline">Reši naš kviz</span>
         </h3>
       </div>
       <h1 className="font-bold text-4xl">Raziščite katalog tečajev</h1>
       <div className="mt-8">
         <h2 className="font-semibold text-2xl">Popularni tečaji in jeziki</h2>
-        <div className="flex flex-row gap-4 mt-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
           {Array.from({ length: 4 }).map((_, index) => {
             return (
               <div
-                className="bg-neutral-900 border-neutral-800 rounded-xl flex-1 h-[150px]"
+                className="bg-neutral-900 border-neutral-700 border  flex flex-col justify-between text-neutral-100 rounded-xl p-4 md:flex-1 w-full"
                 key={index}
-              ></div>
+              >
+                <div className="space-y-2">
+                  <h2 className="font-semibold text-xl">
+                    Lorem ipsum dolor sit amet
+                  </h2>
+                  <p>
+                    {String(`Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Architecto, odio quas. Enim officia ratione quos ea dicta
+                    error rem, dolore quasi, pariatur beatae, quia esse voluptas
+                    omnis voluptatum architecto accusamus!`).slice(
+                      0,
+                      Math.floor(Math.random() * 200),
+                    )}
+                  </p>
+                  <Badge className="bg-neutral-100 text-neutral-900">
+                    {Math.floor(Math.random() * 100) % 2 == 0
+                      ? "C++"
+                      : "Python"}
+                  </Badge>
+                </div>
+                <div className="flex flex-row items-center gap-2 mt-8">
+                  {" "}
+                  <Dumbbell /> <p>{"Begginer"}</p>
+                </div>
+              </div>
             );
           })}
         </div>
