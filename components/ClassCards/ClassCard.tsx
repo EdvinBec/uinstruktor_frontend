6de6c8c7ac8d2ff31e9836df9383fea437d11a76
alkrajnc <aljaz.krajnc@sers.si>
2024-02-23 17:12:24 +0100

    rework: class card, assigment card, class landing page

--- components/ClassCards/ClassCard.tsx
+++ components/ClassCards/ClassCard.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
 import React from "react";
-import Placeholder from "@/assets/img/gradient2.jpg";
-import Placeholder2 from "@/assets/img/gradient.jpg";
 import { Progress } from "@/components/ui/progress";
 import { Label } from "@/components/ui/label";
 import { Button } from "../ui/button";
+
+import Banner0 from "@/assets/img/gradient.jpg";
+import Banner1 from "@/assets/img/gradient2.jpg";
+
+import Image from "next/image";
+import { ArrowRight } from "lucide-react";
 const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 type Props = {
@@ -17,23 +20,19 @@ type Props = {
 const ClassCard = ({ title, description, src, progress }: Props) => {
   return (
     <div className="w-full h-full min-h-[200px] rounded-xl border border-gray-200 hover:shadow-2xl shadow-lg transition-all duration-300">
-      <div
-        className="rounded-t-xl min-h-[100px] flex flex-col justify-end p-4 bg-center bg-cover
-        bg-no-repeat"
-        style={{ backgroundImage: "url('/gradient.jpg')" }}
-      >
-        <h1 className="text-xl self-start font-bold">{title}</h1>
+      <div className="rounded-t-xl relative min-h-[100px] flex flex-col justify-end p-4">
+        <Image
+          className="absolute w-full h-[100px] -z-20 top-0 left-0 rounded-t-xl inset-0"
+          src={Math.floor(Math.random() * 100) % 2 === 0 ? Banner0 : Banner1}
+          alt="Class Banner Image"
+        />
+        <h1 className="text-xl text-white self-start font-bold">{title}</h1>
+        <p className="text-neutral-300 text-sm">{"Author name"}</p>
       </div>
-      <div className="p-4 flex min-h-[200px] flex-col justify-between mt-1">
+      <div className="p-4 flex min-h-[150px] flex-col justify-between mt-1">
         <p className="text-neutral-600 font-medium">{description}</p>
-        <div className="">
-          <div className="flex flex-row justify-between items-center">
-            <span>Progress</span> <span>{progress}%</span>
-          </div>
-          <Progress value={progress} />
-        </div>
         <Button variant={"blue"} className="flex-[0] self-end">
-          Vstopi
+          <ArrowRight />
         </Button>
       </div>
     </div>
