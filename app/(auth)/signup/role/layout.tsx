01f5719ea391f5176400c167c52877ac30dc4759
alkrajnc <aljaz.krajnc@sers.si>
2024-02-24 18:16:09 +0100

    Removed unused user context

--- app/(auth)/signup/role/layout.tsx
+++
@@ -1,7 +0,0 @@
-import Container from "@/components/ui/Container";
-
-const RoleLayout = ({ children }: { children: React.ReactNode }) => {
-  return <Container>{children}</Container>;
-};
-
-export default RoleLayout;
