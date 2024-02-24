2626ba4f18e507d3a5ed76288cffe8831de108ec
Edvin Becic <edvinbecic16@outlook.com>
2024-02-24 23:05:57 +0100

    fix

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -4,12 +4,8 @@ import Container from "@/components/ui/Container";
 
 const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col h-full min-h-screen bg-noise">
-      <Container>
-        <Navbar />
-        {children}
-        <Footer />
-      </Container>
+    <div className="flex flex-col h-screen bg-noise">
+      <Container>{children}</Container>
     </div>
   );
 };
