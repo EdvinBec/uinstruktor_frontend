afd294129e324f8095ebc8f797b6e4fbb6a7e339
Edvin Becic <edvinbecic16@outlook.com>
2024-02-25 12:58:25 +0100

    add new logo

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,18 +1,13 @@
 import { Copyright } from "lucide-react";
 import { Button } from "../ui/button";
 import Link from "next/link";
+import Logo from "../Logo";
 
 const Footer = () => {
   return (
     <div className="flex justify-between mt-4 w-full lg:min-w-[1100px] bg-white dark:bg-black px-4 py-2 border-[1px] border-gray-200 dark:border-0 border-b-0 rounded-t-md">
-      <div className="flex gap-2 items-center">
-        <Copyright size={18} />
-        <p className="text-xs md:text-sm font-medium mr-4">
-          UInstruktor{" "}
-          <span className="font-bold hidden md:inline-block">
-            {new Date().getFullYear()}
-          </span>
-        </p>
+      <div className="flex items-center gap-8">
+        <Logo classname="w-[125px]" />
         <div className="hidden md:flex gap-4">
           <Button variant="link" className="p-0 h-auto">
             <Link href="/explore">Explore</Link>
