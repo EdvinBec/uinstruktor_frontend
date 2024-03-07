e38969fc1e310237ccdf644dce09b554c23bd65b
alkrajnc <aljaz.krajnc@sers.si>
2024-03-07 12:58:23 +0100

    changed difficulty icons and added tooltip to exercises

--- app/(default)/exercises/components/Exercise.tsx
+++ app/(default)/exercises/components/Exercise.tsx
@@ -1,6 +1,13 @@
 import { Badge } from "@/components/ui/badge";
 import { Label } from "@/components/ui/label";
-import { Coins, Dumbbell } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import { Coins, Dumbbell, Info, Weight } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
@@ -21,9 +28,19 @@ const Exercise = ({
   tags,
   exerciseID,
 }: Props) => {
+  let weightSize;
+
+  if (difficulty === "lahko") {
+    weightSize = 17;
+  } else if (difficulty === "srednje") {
+    weightSize = 25;
+  } else {
+    weightSize = 30;
+  }
+
   return (
     <Link className="h-[200px]" href={`/exercise/${exerciseID}`}>
-      <div className="min-h-[200px] w-full flex flex-col justify-between border-gray-200 dark:border-0 border-[1px] rounded-md p-4 bg-white dark:bg-black">
+      <div className="min-h-[220px] w-full flex flex-col justify-between border-gray-200 dark:border-0 border-[1px] rounded-md p-4 bg-white dark:bg-black">
         <div>
           <h3 className="font-bold text-lg">{title}</h3>
           <p className="text-sm mt-1">{description}</p>
@@ -41,12 +58,26 @@ const Exercise = ({
           </div>
           <div className="flex flex-row justify-between pb-1">
             <div className="flex flex-row items-center gap-2">
-              <Dumbbell size={20} strokeWidth={2} />
+              <Weight size={weightSize} strokeWidth={2} />
               <Label className="capitalize">{difficulty}</Label>
             </div>
             <div className="flex flex-row items-center gap-2">
               <Coins size={20} strokeWidth={2} />
               <Label className="text-sm">{points}</Label>
+
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <div className="hover:text-blue">
+                      <Info size={18} />
+                    </div>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Tekmuj z drugimi v zbiranju točk.</p>
+                    <p>Preglej lestvico najboljših učencev</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
             </div>
           </div>
         </div>
